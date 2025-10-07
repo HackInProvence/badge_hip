d1d6bfab7a7bb5e1dfcb80df5e5f22cd9c2bb27b
Miaou <miaou.pbl@gmail.com>
2025-10-07 11:10:56 +0200

    Add templates for new libraries

---
+++ src/template/template.c
@@ -0,0 +1,6 @@
+/* badge_secsea © 2025 by Hack In Provence is licensed under
+ * Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International.
+ * To view a copy of this license,
+ * visit https://creativecommons.org/licenses/by-nc-sa/4.0/ */
+
+#include "template.h"
---
+++ src/tests/template.c
@@ -0,0 +1,15 @@
+/* badge_secsea © 2025 by Hack In Provence is licensed under
+ * Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International.
+ * To view a copy of this license,
+ * visit https://creativecommons.org/licenses/by-nc-sa/4.0/ */
+
+#include <stdio.h>
+
+#include "pico/stdlib.h"
+
+#include "template.h"
+
+
+int main() {
+    stdio_usb_init();
+}
